04c571d2040fac115f80895474fce6bcb697e1cf
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-09-12 22:20:47 +0600

    final

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBnFB2S7_agVEc6ZKK_ePuSOn5lmrc_xmY",
+    authDomain: "burj-al-arab-5a47c.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-5a47c.firebaseio.com",
+    projectId: "burj-al-arab-5a47c",
+    storageBucket: "burj-al-arab-5a47c.appspot.com",
+    messagingSenderId: "353953041595",
+    appId: "1:353953041595:web:36946ebe157e233604ed9e"
+  };
+
+  export default firebaseConfig;
